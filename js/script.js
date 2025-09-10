0721bc13f9485bbd34c3f02408a784b83331d28d
BaptistDeBoodt <baptist.deboodt@gmail.com>
2025-09-10 22:10:45 +0200

    responsive

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
 const projects = [
   { 
     title: "Gladiator (video)",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    tags: "React, Node.js, Express, MongoDB",
+    description: "Gladiator was my vision of what a boxing webshop should look like. I wanted it to have a rugged and cold look. It was also my chance to develop my React.js skills. The exercise was simple: I had to immerse myself in the world of boxing webshops and find inspiration. I was then expected to develop a complete design and finally convert it into React code. The result was Gladiator, where lions are tamed!",
+    tags: "React, MongoDB, Responsive",
     image: "../images/gladiator.png",
     link: "https://drive.google.com/file/d/1BKb1Szz2_mvIKv6OkkaZXGPOyfvuJfzO/view?usp=sharing"
   },
+    {
+    title: 'Portfolio',
+    description: 'During my studies, I had the opportunity to immerse myself in the world of 3D. The aim was to create a 3D portfolio in a creative way. My final version is a horror-themed journey through all my projects, preceded by a point-and-click game. Play with the sound on (if you dare)!',
+    tags: 'Three.js, React, Blender',
+    image: "../images/horrormansion.png",
+    link: "https://3-d-portfolio-five-omega.vercel.app/"
+  },
+  {
+    title: 'Tower VS Cube',
+    description: 'Tower versus cube is a 3D game that I developed using Three.js. The aim was to demonstrate that we were capable of creating a small video game. Enjoy playing!',
+    tags: 'Three.js, React',
+    image: "../images/tower-vs-cube.png",
+    link: "https://tower-vs-cube.vercel.app/"
+  },
   {
     title: "Tetris",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    tags: "React, Node.js, Express, MongoDB",
+    description: "Tetris was my first time working with gsap. It was a fun little project to dip my toes in the water. It's nothing special, but still fun.",
+    tags: "JavaScript, HTML, CSS, GSAP",
     image: "../images/tetris.JPG",
     link: "../projects/eindopdracht/index.html"
   },
@@ -28,13 +42,6 @@ const projects = [
     link: "https://offf-festival-red.vercel.app/"
   },
   {
-    title: 'Love Jet (video)',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    tags: '',
-    image: "../images/lovejet.JPG",
-    link: "https://drive.google.com/file/d/1TDbkPgSG3-oma17Jfg396JSSREaUyttr/view?usp=sharing"
-  },
-  {
     title: 'Guitarz (video)',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     tags: '',
@@ -47,6 +54,13 @@ const projects = [
     tags: '',
     image: "../images/waves.JPG",
     link: "../projects/Waves/index.html"
+  },
+    {
+    title: 'Love Jet (video)',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    tags: '',
+    image: "../images/lovejet.JPG",
+    link: "https://drive.google.com/file/d/1TDbkPgSG3-oma17Jfg396JSSREaUyttr/view?usp=sharing"
   },
   {
     title: 'Pythagoras',
@@ -68,20 +82,6 @@ const projects = [
     tags: '',
     image: "../images/versace.JPG",
     link: "../projects/opdracht 1/index.html"
-  },
-  {
-    title: 'Portfolio',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    tags: '',
-    image: "../images/horrormansion.png",
-    link: "https://3-d-portfolio-five-omega.vercel.app/"
-  },
-  {
-    title: 'Tower VS Cube',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    tags: '',
-    image: "../images/tower-vs-cube.png",
-    link: "https://tower-vs-cube.vercel.app/"
   },
 ];
 
